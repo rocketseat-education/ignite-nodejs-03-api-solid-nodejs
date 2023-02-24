8c2766cbbf907d8861fcfaecfffc6e8d6c619aec
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-02-24 12:38:55 -0300

    Integração com Prisma ORM - Fundamentos do Prisma ORM

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Diego Fernandes',
+    email: 'diego@rocketsaeet.com.br',
+  },
+})
+
+// ORM - Object Relational Mapper
