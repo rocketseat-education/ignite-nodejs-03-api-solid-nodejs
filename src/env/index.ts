c37d7e39291cdf550f301fc01eaa7bac8b088cd5
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-02-24 10:38:06 -0300

    Estrutura do projeto - Carregando variáveis ambiente

---
+++ src/env/index.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+  PORT: z.coerce.number().default(3333)
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.error('❌ Invalid environment variables', _env.error.format())
+
+  throw new Error('Invalid environment variables.')
+}
+
+export const env = _env.data
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import { app } from "./app";
+import { env } from "./env";
 
 app.listen({
   host: '0.0.0.0',
-  port: 3333,
+  port: env.PORT,
 }).then(() => {
   console.log('🚀 HTTP Server Running!')
 })
