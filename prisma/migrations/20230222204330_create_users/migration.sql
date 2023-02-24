2bc807155db7a62e8ce07a91f199abfa11a13fa4
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-02-24 13:08:36 -0300

    Integração com Prisma ORM - PostgreSQL com Docker

---
+++ prisma/migrations/20230222204330_create_users/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
