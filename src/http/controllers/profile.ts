6fcb3f633253b71dd9a0c7cf4bea50fc431aadfc
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-03-06 16:28:43 -0300

    Controllers & Testes E2E - Controller de perfil

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,9 +1,17 @@
+import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use.case'
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  await request.jwtVerify()
+  const getUserProfile = makeGetUserProfileUseCase()
 
-  console.log(request.user.sub)
+  const { user } = await getUserProfile.execute({
+    userId: request.user.sub,
+  })
 
-  return reply.status(200).send()
+  return reply.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined,
+    },
+  })
 }
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,11 +2,12 @@ import { FastifyInstance } from 'fastify'
 import { authenticate } from '@/http/controllers/authenticate'
 import { register } from './controllers/register'
 import { profile } from '@/http/controllers/profile'
+import { verifyJwt } from '@/http/middlewares/verify-jwt'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
 
   /** Authenticated */
-  app.get('/me', profile)
+  app.get('/me', { onRequest: [verifyJwt] }, profile)
 }
