deefe2c60437248dcd5d1b261b3b75f9dc8505a8
André Luiz <adventureandre@hotmail.com>
2025-04-27 20:20:42 -0300

    fix(middleware): adicionar verificação de cabeçalho de autorização no verifyJwt

--- src/http/middlewares/verify-jwt.ts
+++ src/http/middlewares/verify-jwt.ts
@@ -2,8 +2,15 @@ import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function verifyJwt(request: FastifyRequest, reply: FastifyReply) {
   try {
+
+    const authHeader = request.headers.authorization
+    if (!authHeader) {
+      return reply.status(401).send({ message: 'Token não fornecido.' })
+    }
+
     await request.jwtVerify()
   } catch (err) {
     return reply.status(401).send({ message: 'Unauthorized.' })
   }
 }
+
